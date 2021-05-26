3d8fe2c29f729e91efbba2d115c4bfe435ab4451
Olivier ROLAND <newca12@users.noreply.github.com>
2021-05-26 19:48:13 +0200

    Adapt web scraping code to the new IMDb site

--- web-scrapping/src/bin/imdb.rs
+++ web-scrapping/src/bin/imdb.rs
@@ -10,7 +10,23 @@ fn high_contain<'a>(mut strings: impl Iterator<Item = &'a str>, key: &'a str) ->
     strings.any(|item| key.contains(item))
 }
 
-fn get_info(imdb_id: &str) -> (Option<f64>, bool) {
+fn get_info(imdb_id: &str) -> (Option<f64>, Option<bool>) {
+    let mut url = "https://www.imdb.com/title/".to_string();
+    write!(url, "{}", imdb_id).unwrap();
+    let resp = reqwest::blocking::get(url).unwrap();
+    assert!(resp.status().is_success());
+    let body = resp.text().unwrap();
+
+    //For debugging purpose only
+    //let mut debug_file = "/tmp/debug".to_string();
+    //write!(debug_file, "{}", imdb_id).unwrap();
+    //std::fs::write(debug_file, body.clone()).unwrap();
+
+    let fragment = Html::parse_document(&body);
+    get_info_with_fallback(fragment, false)
+}
+
+fn get_info_with_fallback(fragment: Html, fallback: bool) -> (Option<f64>, Option<bool>) {
     let not_theatrical = [
         "TV Movie",
         "TV Short",
@@ -20,39 +36,58 @@ fn get_info(imdb_id: &str) -> (Option<f64>, bool) {
         "TV Special",
     ];
 
-    let mut url = "https://www.imdb.com/title/".to_string();
-    write!(url, "{}", imdb_id).unwrap();
-    let resp = reqwest::blocking::get(url).unwrap();
-    assert!(resp.status().is_success());
-
-    let body = resp.text().unwrap();
-    let fragment = Html::parse_document(&body);
-
-    let rating_selector =
-        Selector::parse(".ratingValue > strong:nth-child(1) > span:nth-child(1)").unwrap();
+    let rating_selector = match fallback {
+        false => Selector::parse(".AggregateRatingButton__RatingScore-sc-1il8omz-1").unwrap(),
+        true => Selector::parse(".ratingValue > strong:nth-child(1) > span:nth-child(1)").unwrap(),
+    };
 
     let rating = fragment.select(&rating_selector).next();
     let rating = rating.map(|r| r.text().next().unwrap());
     let rating = rating.map(|r| r.parse::<f64>().unwrap());
 
-    let theatrical_selector = Selector::parse(".subtext").unwrap();
+    let theatrical_selector = match fallback {
+        false => {
+            Selector::parse(".TitleBlockMetaData__MetaDataList-sc-12ein40-0 > li:nth-child(1)")
+                .unwrap()
+        }
+        true => Selector::parse(".subtext").unwrap(),
+    };
 
     let raw_threatrical = fragment.select(&theatrical_selector).next();
-    let theatrical = raw_threatrical
-        .unwrap()
-        .text()
-        .into_iter()
-        .any(|v| high_contain(std::array::IntoIter::new(not_theatrical), v));
+    let theatrical = raw_threatrical.map(|r| {
+        r.text()
+            .into_iter()
+            .any(|v| high_contain(std::array::IntoIter::new(not_theatrical), v))
+    });
 
-    (rating, theatrical)
+    if rating.is_none() && !fallback {
+        get_info_with_fallback(fragment, true)
+    } else {
+        (rating, theatrical)
+    }
 }
 
 #[test]
 fn get_correct_ratings_and_detect_theatrical_film() {
-    assert!(get_info("tt1390411") == (Some(6.9), false));
-    assert!(get_info("tt0304584") == (Some(4.4), true));
-    assert!(get_info("tt0827521") == (Some(5.5), true));
-    assert!(get_info("tt0001539") == (None, false));
-    assert!(get_info("tt5031232") == (Some(8.8), true));
-    assert!(get_info("tt4049416") == (Some(5.1), true));
+    assert!(
+        get_info("tt1390411") == (Some(6.9), Some(false)),
+        "tt1390411"
+    );
+    assert!(
+        get_info("tt0304584") == (Some(4.4), Some(true)),
+        "tt0304584"
+    );
+    assert!(
+        get_info("tt0827521") == (Some(5.5), Some(true)),
+        "tt0827521"
+    );
+    assert!(get_info("tt0001539") == (None, Some(false)), "tt0001539");
+    assert!(
+        get_info("tt5031232") == (Some(8.8), Some(true)),
+        "tt5031232"
+    );
+    assert!(
+        get_info("tt4049416") == (Some(5.1), Some(true)),
+        "tt4049416"
+    );
 }
