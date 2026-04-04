2d5752adee09bc447f0a331cc1bf83f4732d1a56
Olivier ROLAND <newca12@users.noreply.github.com>
2026-04-04 23:13:38 +0200

    experiment: fast doubling approach

--- src/target.rs
+++ src/target.rs
@@ -1,13 +1,16 @@
 pub fn fibonacci(n: u64) -> u64 {
-    if n == 0 {
-        return 0;
+    fn fib(n: u64) -> (u64, u64) {
+        if n == 0 {
+            return (0, 1);
+        }
+        let (a, b) = fib(n / 2);
+        let c = a.wrapping_mul(b.wrapping_mul(2).wrapping_sub(a));
+        let d = a.wrapping_mul(a).wrapping_add(b.wrapping_mul(b));
+        if n % 2 == 0 {
+            (c, d)
+        } else {
+            (d, c.wrapping_add(d))
+        }
     }
-    let mut a = 0;
-    let mut b = 1;
-    for _ in 2..=n {
-        let temp = a + b;
-        a = b;
-        b = temp;
-    }
-    b
+    fib(n).0
 }
