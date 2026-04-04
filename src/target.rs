9f5ac07dfad15e2bacb3d215d05de593861f0d06
Olivier ROLAND <newca12@users.noreply.github.com>
2026-04-04 23:08:36 +0200

    experiment: iterative approach

--- src/target.rs
+++ src/target.rs
@@ -1,6 +1,13 @@
 pub fn fibonacci(n: u64) -> u64 {
-    if n <= 1 {
-        return n;
+    if n == 0 {
+        return 0;
     }
-    fibonacci(n - 1) + fibonacci(n - 2)
+    let mut a = 0;
+    let mut b = 1;
+    for _ in 2..=n {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    b
 }
